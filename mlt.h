8851c6980c7528721fddb1759a525ccbabb09c08
Alexandre Mulatinho <alex@mulatinho.net>
2017-06-28 16:18:01 -0300

    fixing style

--- mlt.h
+++ mlt.h
@@ -30,20 +30,27 @@ int mlt_result = 0;
 
 struct timeval mlt_init_t, mlt_subinit_t, mlt_end_t, mlt_subend_t;
 
-#define mlt_time_calc(end, ini) (((end.tv_sec - ini.tv_sec) * 1000000UL + end.tv_usec) - ini.tv_usec) / 1000000.0
+#define mlt_time_calc(end, ini) \
+	(((end.tv_sec - ini.tv_sec) * 1000000UL + end.tv_usec) - ini.tv_usec) / 1000000.0
 
 #define mlt_start() do { \
 	gettimeofday(&mlt_init_t, NULL); \
-	fprintf(stdout, ":. Starting test(s) on '%s' at %s", __FILE__, ctime((const time_t *) &mlt_init_t.tv_sec)); \
-	mlt_rsuccess = 0; mlt_rtests = 0; mlt_result = 0; } while(0)
+	fprintf(stdout, ":. Starting test(s) on '%s' at %s", \
+		__FILE__, ctime((const time_t *) &mlt_init_t.tv_sec)); \
+	mlt_rsuccess = 0; mlt_rtests = 0; mlt_result = 0; \
+	} while(0)
 
 #define mlt_finish() do { \
 	gettimeofday(&mlt_end_t, NULL); \
-	mlt_result = mlt_rtests - mlt_rsuccess; if (!(mlt_result)) printf("\n:. Result: PASSED, Time Elapsed: %.3fms, Filename: '%s'\n", mlt_time_calc(mlt_end_t, mlt_init_t), __FILE__); \
+	mlt_result = mlt_rtests - mlt_rsuccess; \
+	if (!(mlt_result)) \
+		printf("\n:. Result: PASSED, Time Elapsed: %.3fms, Filename: '%s'\n", \
+			mlt_time_calc(mlt_end_t, mlt_init_t), __FILE__); \
 	else printf("\n:. Result: FAILED, Time Elapsed: %.3fms, Filename: '%s'\n", \
-		mlt_time_calc(mlt_end_t, mlt_init_t), __FILE__); \
+			mlt_time_calc(mlt_end_t, mlt_init_t), __FILE__); \
 	printf(":. Tests run: %d, Tests PASSED: %d, Tests FAILED: %d\n\n", \
-		mlt_rtests, mlt_rsuccess, (mlt_rtests-mlt_rsuccess)); } while(0)
+		mlt_rtests, mlt_rsuccess, (mlt_rtests-mlt_rsuccess)); \
+	} while(0)
 
 #define mlt_debug(res) \
 	printf("return %s in '%s' on function '%s()' line %d, test '%s'\n", \
@@ -52,11 +59,13 @@ struct timeval mlt_init_t, mlt_subinit_t, mlt_end_t, mlt_subend_t;
 #define mlt_time_end() do { \
 	gettimeofday(&mlt_subend_t, NULL); \
 	printf("  elapsed time in '%s' on function '%s()', time: %.3fms\n", \
-		__FILE__, __func__, mlt_time_calc(mlt_subend_t, mlt_subinit_t)); } while(0)
+		__FILE__, __func__, mlt_time_calc(mlt_subend_t, mlt_subinit_t)); \
+	} while(0)
 
 #define mlt_assert(test) do { \
 	if (test) { mlt_rsuccess++; } \
-	mlt_rtests++; mlt_debug(test); } while(0)
+	mlt_rtests++; mlt_debug(test); \
+	} while(0)
 
 #define mlt_time_init() gettimeofday(&mlt_subinit_t, NULL);
 
